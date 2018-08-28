14bf501f7f121b550a0b9f83a23ee185099d11c6
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-08-28 20:20:11 +0300

    made some changes in homework_data-types

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,69 +1,88 @@
-//#1 type of passed parameter
+//#1
 function findType(x) {
-    if (x === 'false' || x === 'true') {
-        return 'boolean';
-    } else if (x === 'null') {
-        return 'object';
-    } else if (!Number.isNaN(Number(x))) {
-        return 'number';
-    } else {
-        return 'string';
+    return typeof x;
+}
+
+
+//#2
+function forEach(arr, func) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        res.push(func(arr[i]));
     }
+    return res;
 }
 
-// #2 type of passed parameter
-function forEach(arr, some) {
 
-    arr.forEach(some);
-    return arr;
+//#3
+function map(array, func) {
+    return forEach(array, func);
 }
+// console.log(map(arr1, x=>x+3));
 
-//#3 returns transformed array
-function map(num, index, arr) {
-    let cf = 3;
-    arr[index] = num + cf;
-    return arr;
-}
+
 
 //#4
-function filter(num, index, arr) {
-    let cf = 3;
-    let one = 1;
-    if (num < cf) {
-        let x = arr.indexOf(num);
-        arr.splice(x, one);
+function filter(array, func) {
+    let res = [];
+
+    let passedFilter = forEach(array, func);
+
+    for (let i = 0; i < array.length; i++) {
+        if (passedFilter[i] === true) {
+            res.push(array[i]);
+        }
     }
-    return arr;
+
+    return res;
 }
 
 
 //#5
-function getAdultAppleLovers(item, i, arr) {
-    if (arr[i]['age'] > 18 && arr[i].favoriteFruit === 'apple') {
-        let ar2 = [];
-        ar2.push(arr[i].name);
-        console.log(ar2);
-    }
-
+function getAdultAppleLovers(x) {
+    let num=18;
+    return x.age > num && x.favoriteFruit=== 'apple';
+}
+function getName(person) {
+    return person.name;
 }
 
-//#6 returns array of keys
+// let res=filter(data,getAdultAppleLovers);
+// console.log(map(res,getName));
+
+
+
+//#6
 function keys(arr) {
-    return Object.keys(arr);
+    let res = [];
+    for (let key in arr) {
+        if (arr.hasOwnProperty(key)) {
+            res.push(key);
+        }
+    }
+    return res;
 }
 
-//#7 returns array of values
+
+// //#7
 function values(arr) {
-    return Object.values(arr);
+    let res = [];
+    for (let key in arr) {
+        if (arr.hasOwnProperty(key)) {
+            res.push(arr[key]);
+        }
+    }
+    return res;
 }
 
-//#8 formatted date
+
+//#8
 function showFormattedDate(date) {
     let months = [
-        'January', 'February', 'March',
-        'April', 'May', 'June', 'July',
-        'August', 'September', 'October',
-        'November', 'December'
+        'Jan', 'Feb', 'Mar',
+        'Apr', 'May', 'Jun',
+        'Jul','Aug', 'Sep',
+        'Oct', 'Nov', 'Dec'
     ];
 
     let day = date.getDate();
