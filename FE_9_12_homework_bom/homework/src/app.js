290bd9675c668a137ee4f5a9ef3176e495e57356
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-09-09 21:26:51 +0300

    made some changes

--- FE_9_12_homework_bom/homework/src/app.js
+++ FE_9_12_homework_bom/homework/src/app.js
@@ -1,18 +1,31 @@
-const rootNode = document.getElementById('root');
-
-const todoItems = [
-    {isDone: false, id: 12345, description: 'Todo 1'}
-];
-
+let todoList = [];
 let ul = document.getElementById('ul');
-let list1 = 'Find a Cat';
-let list2 = 'Find another a Cat';
-let list3 = 'Find another a Cat 2';
+let item1 = 'Find a Cat';
+let item2 = 'Find another a Cat';
+let item3 = 'Find another a Cat 2';
 
-createElement(list1);
-createElement(list2);
-createElement(list3);
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+
+}
+
+let form = document.getElementById('form1');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let inputText = document.getElementById('addNewForm').value;
+    if (!inputText) {
+        console.log('empty');
+    } else {
+        todoList = newItem(inputText);
+        returnPage('#');
+    }
+});
+
+
+//Changing page functions
 function changePage() {
 
     let todo = document.getElementById('todo');
@@ -31,65 +44,53 @@ function changePage() {
     }
 }
 
-let form = document.getElementById('form1');
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    let inputText = document.getElementById('addNewForm').value;
-    if (!inputText) {
-        console.log('empty');
-    } else {
-        createElement(inputText);
-        returnPage('#');
-    }
-});
-
 function ChangeHref(x) {
     location.hash = x;
     console.log(x);
 }
 
 function returnPage(x) {
-    location.href = location.href.substr(0, window.location.href.indexOf(x));
-    // console.log('returnPage');
+    let zero = 0;
+    location.href = location.href.substr(zero, window.location.href.indexOf(x));
     // console.log( location.href);
 
 }
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Включно з мінімальним та виключаючи максимальне значення
-
+function newItem(text) {
+    let maxId = 50000;
+    let id = getRandomInt(1, maxId);
+    localStorage.setItem(id, text);
+    createElement(id);
 }
 
-function createElement(text) {
+function createElement(id) {
 
+    let text = localStorage.getItem(id);
     let item = document.createElement('li');
     let textPart = document.createElement('p');
     textPart.innerHTML = text;
-    textPart.addEventListener('click', function () {
-        location.hash = '/modifyItem';
-
-    });
-    let todoIcon = document.createElement('img');
-    todoIcon.src = './assets/img/todo-s.png';
-    todoIcon.className = 'todoIcon';
-    todoIcon.addEventListener('click', function () {
-        doneIcon.style.display = 'inline-block';
-        todoIcon.style.display = 'none';
-    });
-    let doneIcon = document.createElement('img');
-    doneIcon.src = './assets/img/done-s.png';
-    doneIcon.className = 'doneIcon';
-    let removeIcon = document.createElement('img');
-    removeIcon.src = './assets/img/remove-s.jpg';
-    removeIcon.addEventListener('click', function () {
-        item.remove()
-    });
-    item.appendChild(todoIcon);
-    item.appendChild(doneIcon);
+    alert(textPart.innerHTML);
+    // textPart.addEventListener('click', function () {
+    //     location.hash = '/modifyItem';
+    // });
+    // let todoIcon = document.createElement("img");
+    // todoIcon.src = './assets/img/todo-s.png';
+    // todoIcon.className = 'todoIcon';
+    // todoIcon.addEventListener('click', function () {
+    //     doneIcon.style.display = 'inline-block';
+    //     todoIcon.style.display = 'none';
+    // });
+    // let doneIcon = document.createElement("img");
+    // doneIcon.src = './assets/img/done-s.png';
+    // doneIcon.className = 'doneIcon';
+    // let removeIcon = document.createElement("img");
+    // removeIcon.src = './assets/img/remove-s.jpg';
+    // removeIcon.addEventListener('click', function () {
+    //     item.remove()
+    // });
+    // item.appendChild(todoIcon);
+    // item.appendChild(doneIcon);
     item.appendChild(textPart);
-    item.appendChild(removeIcon);
+    // item.appendChild(removeIcon);
     ul.appendChild(item);
-
 }
