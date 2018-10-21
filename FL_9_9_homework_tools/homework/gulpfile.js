7b842b18461939dcd2d1e70705cb60d21daf487b
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-10-21 18:47:41 +0300

    Made adding js and links to html

--- FL_9_9_homework_tools/homework/gulpfile.js
+++ FL_9_9_homework_tools/homework/gulpfile.js
@@ -6,10 +6,17 @@ sass.compiler = require('node-sass');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const del = require('del');
+const inject = require('gulp-inject');
+const babel = require('gulp-babel');
 
+gulp.task('babel', () =>
+    gulp.src('src/js/*.js')
+        .pipe(babel({presets: ['@babel/env']}))
+        .pipe(gulp.dest('dist/js'))
+);
 
 gulp.task('compressJs', function(){
-    return gulp.src('src/js/*.js')
+    return gulp.src('dist/js/*.js')
         .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
@@ -34,11 +41,19 @@ gulp.task('copyHtml', function () {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('src/sass/*.scss', ['sass']);
-});
-
 gulp.task('del', function () {
     del(['dist/*.min.js', 'dist/*.min.css']);
 });
 
+gulp.task('addCssJs', function () {
+    let sources = gulp.src(['dist/*.min.js','dist/*.min.css'], {read: false});
+
+    return gulp.src('./src/app.html')
+        .pipe(inject(sources))
+        .pipe(gulp.dest('./src'));
+});
+
+
+gulp.task('sass:watch', function () {
+    gulp.watch('src/sass/*.scss', ['sass']);
+});
