6e1fc1643a4c66d684a7b71fc7d89699024b5bf5
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-10-21 21:27:21 +0300

    added build  and build-prod

--- FL_9_9_homework_tools/homework/gulpfile.js
+++ FL_9_9_homework_tools/homework/gulpfile.js
@@ -8,21 +8,30 @@ const rename = require('gulp-rename');
 const del = require('del');
 const inject = require('gulp-inject');
 const babel = require('gulp-babel');
+const connect = require('gulp-connect');
 
-gulp.task('babel', () =>
+const runSequence = require('run-sequence');
+
+const gulpif = require('gulp-if');
+const argv = require('yargs').argv;
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('compileJs', () =>
     gulp.src('src/js/*.js')
         .pipe(babel({presets: ['@babel/env']}))
         .pipe(gulp.dest('dist/js'))
 );
 
 gulp.task('compressJs', function(){
-    return gulp.src('dist/js/*.js')
+    return gulp.src(['node_modules/moment/moment.js', 'dist/js/clock.js', 'dist/js/canvasState.js', 'dist/js/app.js'])
         .pipe(concat('app.min.js'))
+        .pipe(gulpif((!argv.production), sourcemaps.init()))
         .pipe(uglify())
+        .pipe(gulpif((!argv.production), sourcemaps.write()))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('scssToCss', function () {
+gulp.task('compileCss', function () {
     return gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist/css'));
@@ -45,7 +54,7 @@ gulp.task('del', function () {
     del(['dist/*.min.js', 'dist/*.min.css']);
 });
 
-gulp.task('addCssJs', function () {
+gulp.task('addCssJsToHtml', function () {
     let sources = gulp.src(['dist/*.min.js','dist/*.min.css'], {read: false});
 
     return gulp.src('./src/app.html')
@@ -53,7 +62,23 @@ gulp.task('addCssJs', function () {
         .pipe(gulp.dest('./src'));
 });
 
-
 gulp.task('sass:watch', function () {
     gulp.watch('src/sass/*.scss', ['sass']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'src',
+        port: 8080
+    });
+});
+
+gulp.task('default', ['connect']);
+
+gulp.task('build', function () {
+    runSequence('del', ['compileCss', 'compileJs'],['compressJs', 'compressCss'],'addCssJsToHtml');
+});
+
+gulp.task('build-prod', function () {
+    runSequence('del', ['compileCss', 'compileJs'],['compressJs', 'compressCss'],'addCssJsToHtml', 'copyHtml');
 });
