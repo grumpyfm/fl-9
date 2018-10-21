49ddaaa5505c86f0afc7011a882573c650084eb3
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-10-21 18:04:56 +0300

    Made scssToCss,compressCss,copyHtml

--- FL_9_9_homework_tools/homework/gulpfile.js
+++ FL_9_9_homework_tools/homework/gulpfile.js
@@ -1,14 +1,44 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
-const pump = require('pump');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const del = require('del');
 
 
-gulp.task('compress', function (cb) {
-    pump([
-            gulp.src('src/js/*.js'),
-            uglify(),
-            gulp.dest('dist')
-        ],
-        cb
-    );
+gulp.task('compressJs', function(){
+    return gulp.src('src/js/*.js')
+        .pipe(concat('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('scssToCss', function () {
+    return gulp.src('src/sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('compressCss', function(){
+    return gulp.src('dist/css/*.css')
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copyHtml', function () {
+    return gulp.src('src/app.html')
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('src/sass/*.scss', ['sass']);
+});
+
+gulp.task('del', function () {
+    del(['dist/*.min.js', 'dist/*.min.css']);
+});
+
