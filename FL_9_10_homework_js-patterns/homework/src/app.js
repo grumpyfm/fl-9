3f2ea60457242f67c63f89067ca1d40bc0c494c2
Ludmila Pomohaibo <pomohaibo@gmail.com>
2018-10-24 15:12:41 +0300

    Added homework_js-patterns

---
+++ FL_9_10_homework_js-patterns/homework/src/app.js
@@ -0,0 +1,38 @@
+class Store {
+    constructor() {
+        this.price = 20;
+        this.nightDiscount = 0;
+        this.weekendDiscount = 0;
+        this.toPay = 0;
+        this.bonus = 0;
+    }
+
+    count() {
+        this.toPay = this.price - this.nightDiscount - this.weekendDiscount;
+    }
+
+    buyPizzaSlice() {
+        return `Price after discount is ${this.toPay} and you have ${this.bonus} bonuses`;
+    }
+}
+
+const getDiscount = obj => {
+    let data = new Date();
+    if (data.getHours() <= 13 || data.getHours() === 23) {
+        obj.nightDiscount = 5;
+    }
+    if (data.getDay() === 0 || data.getDay() === 6) {
+        obj.weekendDiscount = 2;
+    }
+    obj.count();
+    setBonus(obj);
+};
+
+const setBonus = obj => {
+    obj.bonus = obj.bonus + Math.floor(obj.toPay / 10);
+
+};
+
+const test = new Store();
+getDiscount(test);
+console.log(test.buyPizzaSlice());
